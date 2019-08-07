ed99c63251df722a2c496a554bc84be9ac831f81
Florian <florianznottka@eleven.fritz.box>
2019-08-07 20:46:56 +0200

    get und post

--- serverstatusboard.py
+++ serverstatusboard.py
@@ -1,7 +1,21 @@
-from flask import Flask, request
-# set the project root directory as the static folder, you can set others.
-app = Flask(__name__, static_url_path='')
+from flask import Flask, request, redirect, url_for, jsonify
+app = Flask(__name__)
+
 
 @app.route('/')
 def root():
-    return app.send_static_file('index.html')
+    return redirect(url_for('static', filename='index.html'))
+
+
+@app.route('/status/<int:num>', methods=['GET'])
+def get_status(num):
+    return jsonify({'working_status': free})
+
+@app.route('/set_status/<int:num>', methods=['POST'])
+def set_status(num):
+    if (request.method == 'POST'):
+        some_json = request.get_json()
+        return jsonify({'set_status': some_json}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
